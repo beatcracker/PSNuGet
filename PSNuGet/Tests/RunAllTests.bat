addadcf8f8875738c24b8516509d96f1055e6196
beatcracker <beatcracker@users.noreply.github.com>
2016-04-17 01:18:02 +0300

    -NoProfile for tests

--- PSNuGet/Tests/RunAllTests.bat
+++ PSNuGet/Tests/RunAllTests.bat
@@ -1,3 +1,4 @@
 @echo off
+powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "[Console]::SetBufferSize(1000, 3000)"
 powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Import-Module -Name Pester ; Invoke-Pester -Path '%~dp0'"
 pause
